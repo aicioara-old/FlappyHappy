e14501f9303a8f7bca5fb4fad5e81035bf97166b
Joshua Coutinho <jrc12@imperial.ac.uk>
2014-02-09 23:21:48 +0000

    Added ai however results are inconsistent

--- python/main.py
+++ python/main.py
@@ -1,9 +1,11 @@
+import sys
 import autopy
 import time
 import os
 
 def jump():
-	autopy.key.tap(' ', 0)
+	autopy.mouse.click()
+
 
 def loadBirds():
 	
@@ -28,7 +30,7 @@ def getBirdPosition():
 	position = screen.find_bitmap(feather, 0.02)
 	return position
 
-def getPipePosition(birdPos):
+def getPipePosition(birdPos, prev):
 	global screen 
 
 	x = birdPos[0]
@@ -41,6 +43,7 @@ def getPipePosition(birdPos):
 		if not autopy.screen.point_visible(x, y) :
 			return None
 
+	
 	#get to the top of the screen
 	while screen.get_color(x, y) == 7390926:
 		y -=1
@@ -48,11 +51,23 @@ def getPipePosition(birdPos):
 			return None
 	y+=1
 
+	# if the bird is under the pipe then return the 
+	# bounds for the previous pair
+	if prev != None and screen.get_color(birdPos[0], prev[0][1] - 2) != 7390926:
+		return prev
+
 	#get to the top of the pipe
 	while screen.get_color(x, y) == 7390926:
 		x += 1
 		if not autopy.screen.point_visible(x, y) :
 			return None
+
+	# if the bird has not yet crossed the bounds of the previous top pipe then uodate only x
+	if prev != None and prev[0][0] + 60 > birdPos[0]:
+		newTop = (x, prev[0][1])
+		newBottom = (x, prev[1][1])
+		return (newTop, newBottom)
+
 
 	#get to the bottom of the pipe
 	while screen.get_color(x, y) != 7390926:
@@ -63,9 +78,9 @@ def getPipePosition(birdPos):
 	holeTop = (x, y)
 
 	#get bottom of the hole
-	while screen.get_color(x, y) == 7390926:
+	while screen.get_color(x, y) == 7390926 or screen.get_color(x, y)%256 <= 20:
 		y += 1
-		if not autopy.screen.point_visible(x, y) :
+		if not autopy.screen.point_visible(x, y):
 			return None
 
 	holeBottom = (x, y)
@@ -75,33 +90,35 @@ def getPipePosition(birdPos):
 	
 def main():
 	loadBirds()
-
+	tts         = float(sys.argv[1])
+	distBuff    = int(sys.argv[2])
+	updTime     = int(sys.argv[3])
+	thresh      = int(sys.argv[4])
+	errorMargin = int(sys.argv[5])
+	ttj         = int(sys.argv[6])
+	jumpDist    = int(sys.argv[7])
 	x = 0
-
+	pipePos = None
 	while 1:
 		birdPos = getBirdPosition()
-
+		
 		if not birdPos:
 			continue
 
-		pipePos = getPipePosition(birdPos)
+		pipePos = getPipePosition(birdPos, pipePos)
 		if not pipePos:
 			continue
-		
 
-		pipeTop = pipePos[0];
-		pipeBottom = pipePos[1];
+		pipeTop = pipePos[0]
+		pipeBottom = pipePos[1]
 
-		if birdPos[1] < pipeBottom[1]:
-			print "JUMP" + str(x)
-
+		# jump if the bird is below the line 
+		# of the bottom pipe and if a jump 
+		# will not cause it to collide with the
+		# line of the top pipe
+		if pipeBottom[1] <= birdPos[1] + distBuff and pipeTop[1] < birdPos[1] - jumpDist :
+			jump()
 		x += 1
-
-		# print birdPos
-		# print pipeTop
-		# print pipeBottom
-
-		time.sleep(0.5)
 
 
 def screenshot():
